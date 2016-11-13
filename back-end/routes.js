7d8ff5e95435054d0ff08f7120721b0d45927dcc
thayer11 <holly.coddington@gmail.com>
2016-11-13 12:20:46 -0700

    fuck

--- back-end/routes.js
+++ back-end/routes.js
@@ -4,13 +4,9 @@ var bodyParser = require('body-parser');
 var poemController = require('../controllers/poem-Controller');
 
 
-<<<<<<< HEAD
 
-module.exports = router
-=======
 module.exports = router; 
 
 router.get('/', function (req, res) {
    res.send("TODAY");
 })
->>>>>>> 94912fa6074b86a8d9e45e38ae29dd8b4e4a6b77
